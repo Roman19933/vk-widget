74e6af998164bab6b5da36c1d39c2225a2709a4f
Roman <mikhailiuk.roman@gmail.com>
2020-05-31 22:49:22 +0300

    [*]upload api get url2

--- src/store/server/upload.js
+++ src/store/server/upload.js
@@ -25,8 +25,8 @@ export const actions = {
     async getUrl({ commit }, { group_id, image_id }) {
         try {
             let url = await Api[`groups/image/${group_id}/199137986_867747`]()
-            commit('GET_FOTO_URL', url.data.response.images.url)
-            console.log('id', url.data.response.images.url)
+            commit('GET_FOTO_URL', url.data.response[0].images[0].url)
+            console.log('id', url.data.response[0].images[0].url)
         } catch (e) {
             console.log('e', e)
         }
